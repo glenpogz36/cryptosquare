609099669397af9db3c4012fae6893ffa1c07863
Fausto Guillen and Glen Sale <glen@>
2018-10-25 16:21:32 -0700

    add JavaScript/jQuery to encrypt code from user input

--- README.md
+++ README.md
@@ -1,31 +1,18 @@
-# _Leap Year_
+# _Cryptosquare_
 
 #### _Single Webpage for Epicodus, 10.24.2018_
 
-#### By _**Fausto Guillen and Kaveh Saleminejad**_
+#### By _**Fausto Guillen and Glen Sale**_
 
 ## Description
 
-_Specs from Leap Year Lesson
-The program should return when a year is not a leap year.
-Input Example: 1993
-Output Example: false
-The program should return 'true' for years divisible by 4, since those are leap years.
-Input Example: 2004
-Output Example: true
-The program should return 'false' for years divisible by 100, since those are not leap years.
-Input Example: 1900
-Output Example: false
-The program should return 'true' for years divisible by 400, since those are leap years.
-Input Example: 2000
-Output Example: true_
+_Cryptosquare: Write a method to convert a user inputted string into 5 character units of encrypted code. This is accomplished by mapping the string (minus blank spaces and non-alphanumeric characters) onto a grid that is as close to square as possible. To create the encrypted code, the square is read back in the opposite orientation as it was initially mapped. Then a counter is used to add a space every five characters, before the code is printed._
 
 ## Setup/Installation Requirements
 
 * _Clone this repository_
 * _Open a simple HTTP server in the top level of the cloned directory. For a mac, run the following command in your terminal:_   
-* _python -m SimpleHTTPServer_
-* _Then open your web browser of choice to localhost:8000_
+* _Then open your web browser of choice_
 
 ## Technologies Used
 
@@ -39,4 +26,4 @@ Output Example: true_
 
 *This software is licensed under the MIT license.*
 
-Copyright (c) 2018 **_Fausto Guillen and Kaveh Saleminejad_**
+Copyright (c) 2018 **_Fausto Guillen and Glen Sale_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,68 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
-  event.preventDefault();
-    var year = parseInt($("#sentence").val ());
-    var leapYear = false;
-    if (year % 4==0){
-      if (year % 100==0) {
-        if (year % 400==0) {
-          leapYear = true;
-        }
-      } else {
-        leapYear = true;
-      }
-    }
-    if (leapYear){
-      $("#story").append ("<p>This is a leap year</p>");
+	$("#formOne").submit(function() {
+		event.preventDefault();
+		var newString = $("input#inputPhrase").val();
+		newString = newString.replace(/[^a-zA-Z1-9]/g,'').toLowerCase();
+
+		var totalElements = newString.length;
+		var sqRoot = Math.sqrt(totalElements);
+		var columns= Math.floor(sqRoot);
+		var rows= Math.floor(sqRoot);
+
+		if (sqRoot%1 === 0) {
+			columns = sqRoot;
+		  rows = sqRoot;
+		} else if (totalElements>((rows+1)*columns)){
+      columns = (Math.floor(sqRoot)) + 1;
+		  rows = (Math.floor(sqRoot)) + 1;
     } else {
-      $("#story").append ("<p>This is not a leap year</p>");
-    }
-    $("#story").show();
-  });
+			columns = Math.floor(sqRoot);
+		  rows = (Math.floor(sqRoot)) + 1;
+		}
+// 4 columns 4 rows: abcdefghabcdefgh = "aeaeb fbfcg cgdhd h"
+
+		var normalArray = [];
+		var encryptArray = [];
+		var letterCount = 0;
+
+		for (var i=0; i < rows; i++) {
+			var newArray = [];
+		  normalArray[i] = newArray;
+		  //normalArray[i] = new Array(rows);
+
+		  for (var j=0; j < columns; j++) {
+		    normalArray[i][j] = newString.charAt(letterCount);
+		    letterCount++;
+		  }
+		}
+
+		var elementCount = 0;
+
+		for(var i=0; i < columns; i++) {
+			for(var j=0; j < rows; j++) {
+		  	encryptArray[elementCount] = normalArray[j][i];
+		    elementCount++;
+		  }
+		}
+
+		//clear out any empty arrays
+		for (var i=0; i < encryptArray.length; i++) {
+			if (encryptArray[i] === "" ) {
+		  	encryptArray.splice(i,1);
+		  }
+		}
+
+		//add spaces in every fifth element
+		for (var i=0; i < encryptArray.length; i++) {
+			if ((i+1)%5===0) {
+		  	encryptArray[i] = encryptArray[i] + " ";
+		  }
+		}
+
+		var outputString = encryptArray.toString();
+		outputString = outputString.replace(/[,]/g,'');
+		//alert(outputString);
+		$("#outputPhrase").text(outputString);
+		$("#result").show();
+	});
 });
